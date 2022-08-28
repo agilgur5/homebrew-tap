7ebfdd54cb01f8ba4804f58adcd2e8c557ff3327
Anton Gilgur <agilgur5@gmail.com>
2022-08-27 21:06:28 -0400

    feat: add goenv formula with latest commit hash
    - since `goenv` has not released a stable version in a few years, the `core` Formula is pretty out-of-date and missing many newer Go versions
      - since `core` doesn't accept non-stable tags, add this to a third-party tap that can do so (in this case, mine)
      - see also my comment https://github.com/syndbg/goenv/issues/238#issuecomment-1214260116
    
    - diff https://github.com/Homebrew/homebrew-core/blob/8bad09e9fe5453f0daf74eef4825f49925f86bc6/Formula/goenv.rb#
      - changed `url` to use latest commit hash
      - updated `sha256` to match the archive's SHA
      - updated `bottle do` statement to match what I got locally (following https://github.com/Homebrew/brew/blob/master/docs/Bottles.md)
      - remove `unless build.head?` statement since this is `HEAD`

---
+++ Formula/goenv.rb
@@ -0,0 +1,37 @@
+class Goenv < Formula
+  desc "Go version management"
+  homepage "https://github.com/syndbg/goenv"
+  url "https://github.com/syndbg/goenv/archive/f2acd3cf09e374cfd359fc2e3e3f1879ab083d31.tar.gz"
+  sha256 "f57a1c3ebf317fa6a26774e08729e77e3a78c74806ac786ee744a65b9821783f"
+  license "MIT"
+  version_scheme 1
+  head "https://github.com/syndbg/goenv.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, monterey: "5ebf7f8a41fb6b283af2da8a1bcd2079a090d92eaa2c4ba30738d415d6838906"
+  end
+
+  def install
+    inreplace_files = [
+      "libexec/goenv",
+      "plugins/go-build/install.sh",
+      "test/goenv.bats",
+      "test/test_helper.bash",
+    ]
+    inreplace inreplace_files, "/usr/local", HOMEBREW_PREFIX
+
+    prefix.install Dir["*"]
+    %w[goenv-install goenv-uninstall go-build].each do |cmd|
+      bin.install_symlink "#{prefix}/plugins/go-build/bin/#{cmd}"
+    end
+  end
+
+  test do
+    assert_match "Usage: goenv <command> [<args>]", shell_output("#{bin}/goenv help")
+  end
+end
